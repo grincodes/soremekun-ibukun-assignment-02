c9e9f398072dcb63f25227adbb1742ed781b7e1c
ibukunoluwaseyisoremekun <designerseyi@gmail.com>
2022-07-11 08:14:27 +0100

    assignment update

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,16 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+    let sum = 0;
+     if(arrayOfNumbers.length){
+         arrayOfNumbers.map((val)=>{
+             sum += val
+         })
+     }
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +35,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0;
+    if(arrayOfNumbers.length){
+        arrayOfNumbers.map((val)=>{
+            if(val % 2 == 0){
+                sum ++
+            }
+        })
+    }
+   return sum;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
